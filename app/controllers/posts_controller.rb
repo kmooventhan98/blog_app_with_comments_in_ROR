fa22404a72a524c37156a1d0bc869a3a03354417
Mooventhan <mooventhan.healthguru@gmail.com>
2022-10-02 19:28:08 +0530

    before comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,9 +16,28 @@ class PostsController < ApplicationController
         end
     end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit '
+        end
+    end
+
     def show
         @post = Post.find(params[:id])
         puts "@post------"+@post.inspect.to_s
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
     end
 
     private
