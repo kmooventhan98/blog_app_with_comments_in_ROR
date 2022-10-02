6aeb38c0a0750cb09c6cc786d11b44d0db78ee1c
Mooventhan <mooventhan.healthguru@gmail.com>
2022-10-02 21:33:14 +0530

    added comment functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    before_action :find_post, only: [:show,:update, :edit, :destroy]
     def index
         @posts = Post.all.order("created_at DESC")
     end
@@ -17,11 +18,11 @@ class PostsController < ApplicationController
     end
 
     def edit
-        @post = Post.find(params[:id])
+
     end
 
     def update
-        @post = Post.find(params[:id])
+
         if @post.update(post_params)
             redirect_to @post
         else
@@ -30,12 +31,11 @@ class PostsController < ApplicationController
     end
 
     def show
-        @post = Post.find(params[:id])
-        puts "@post------"+@post.inspect.to_s
+
     end
 
     def destroy
-        @post = Post.find(params[:id])
+
         @post.destroy
         redirect_to posts_path
     end
@@ -43,5 +43,8 @@ class PostsController < ApplicationController
     private
     def post_params
         params.require(:post).permit(:title,:content)
+    end
+    def find_post
+        @post = Post.find(params[:id])
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220914171127) do
+ActiveRecord::Schema.define(version: 20221002152453) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
